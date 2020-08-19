cb496f0c14ff051ad4392e99a6d26d19d18d75cc
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-08-19 18:08:11 +0700

    Update auto_test_samples.sh

--- .github/workflows/auto_test_samples.sh
+++ .github/workflows/auto_test_samples.sh
@@ -10,7 +10,7 @@ php sample/CreateIndex.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/CreateTimeseries.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/GetRow.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/PutRow.php 239.0.0.1 31999 griddbubuntu admin admin
-php sample/RemoveRowByRowKey.php 239.0.0.1 31999 griddbubuntu admin admin
+php sample/RemoveRowByRowkey.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/RemoveRowByTQL.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/TimeSeriesRowExpiration.php 239.0.0.1 31999 griddbubuntu admin admin
 php sample/TQLAggregation.php 239.0.0.1 31999 griddbubuntu admin admin
